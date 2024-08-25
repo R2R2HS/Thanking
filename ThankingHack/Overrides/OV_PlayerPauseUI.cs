7dc4dbcf08d3d57a7022f0d8e31e7c3b6f8d1d6d
WrongSell <84908244+R2R2HS@users.noreply.github.com>
2024-08-25 12:55:28 +0300

    fix OV_PlayerPauseUI

--- ThankingHack/Overrides/OV_PlayerPauseUI.cs
+++ ThankingHack/Overrides/OV_PlayerPauseUI.cs
@@ -7,7 +7,6 @@ namespace Thanking.Overrides
     public static class OV_PlayerPauseUI
     {
         [Override(typeof(PlayerPauseUI), "onClickedExitButton", BindingFlags.NonPublic | BindingFlags.Static)]
-        public static void OV_onClickedExitButton(SleekButton button) =>
-            Provider.disconnect();
+        public static void OV_onClickedExitButton(ISleekButton button) => Provider.disconnect();
     }
 }
